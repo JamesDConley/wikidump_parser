5bc1ca0b15f6a376de8e61093e5550777a7e3ad0
James Conley <jconley1@alumni.conncoll.edu>
2024-01-21 11:59:42 -0500

    run full sequence

--- convert.sh
+++ convert.sh
@@ -1,3 +1,6 @@
-python gen_csv.py $1 data/output/base_dump.csv
-python count_mentions.py data/output/base_dump.csv data/output/mention_counts.csv --n_workers=64
-python find_top_articles.py data/output/mention_counts.csv data/output/top_articles.csv --ignore_dupes
+#!/bin/bash
+# Exit on error 
+set -e
+python3 src/gen_csv.py -i $1 -o data/output/base_dump.csv
+python3 src/count_mentions.py data/output/base_dump.csv data/output/mention_counts.csv --n_workers=64
+python3 src/find_top_articles.py data/output/mention_counts.csv data/output/top_articles.csv --ignore_dupes --n_workers=64
